a59b32455542a5e26400d14c9b1296d0637af9f5
mrwindupbirdesq <nmaahs11@gmail.com>
2024-02-17 14:10:55 -0800

    Extract firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,17 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfRyVZfk9LHN2oMF6X5cWpFUJbujNvSMs",
-  authDomain: "wild-roots-vashon.firebaseapp.com",
-  projectId: "wild-roots-vashon",
-  storageBucket: "wild-roots-vashon.appspot.com",
-  messagingSenderId: "636226872750",
-  appId: "1:636226872750:web:0a7154590e82771752543f",
-  measurementId: "G-6XCFSC6GEG"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app, analytics };
