483aef83bdcd7a59bb7ac16dc7a002ef139ab903
mrwindupbirdesq <nmaahs11@gmail.com>
2024-03-17 10:01:40 -0700

    Add and incorporate footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import styles from './footer.module.css';
+
+function Footer() {
+  return (
+    <div className={styles.footerContainer}>
+      <h5>Wild Roots Farm and Kitchen</h5>
+      <p>(503) 473-4349</p>
+      <p>Vashon, WA, United States, Washington</p>
+      <a href="https://www.facebook.com/profile.php?id=100064478480593">
+        <img
+          className={styles.facebookIcon}
+          alt="Facebook"
+          fetchpriority="high"
+          src="https://static.wixstatic.com/media/0fdef751204647a3bbd7eaa2827ed4f9.png/v1/fill/w_40,h_40,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Facebook.png"
+        ></img>
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/PublicLayout.jsx
+++ src/components/PublicLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './Nav/Nav';
+import Footer from './Footer/Footer';
 
 export const PublicLayout = () => {
   return (
     <>
       <Nav />
       <Outlet />
+      <Footer />
     </>
   );
 };
