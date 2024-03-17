bea8c0eb2d1599152d13d4a018a03e71dfa4c8e3
mrwindupbirdesq <nmaahs11@gmail.com>
2024-03-17 11:58:40 -0700

    Add about content, style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,18 +2,32 @@ import { useState, useEffect } from 'react';
 import { db, storage } from '../../firebase-config';
 import { ref as dbRef, onValue as onDbValue } from 'firebase/database';
 import styles from './about.module.css';
-
 function About() {
   return (
     <div className={styles.aboutContainer}>
+      <img
+        className={styles.aboutBannerPhoto}
+        src="https://source.unsplash.com/EwKXn5CapA4"
+        alt=""
+      />
+
+      <h3 className={styles.aboutTitle}>Meet the Owner</h3>
+      <hr />
       <div className={styles.aboutTextContainer}>
         <p>
-          We make nourishing and seasonal cuisine to order from our professional
-          kitchen located on beautiful Vashon Island. We utilize as many local
-          ingredients as possible, some of which are grown on site in the Wild
-          Roots Garden
+          Hi! I am Claudia, founder of Wild Roots Farm and Garden and our
+          recently established Wild Roots Kitchen. I have an over 30 year
+          Culinary background that includes Farm to Table Foods and Artisan
+          Baked Goods. I started my career in the San Francisco Bay Area in the
+          early '90s as a Baker and Personal Chef and then moved to Portland in
+          2003 to purchase a 20 acre farm and establish my Catering Company and
+          Farm. My family moved to Vashon in 2012, and I have been building Wild
+          Roots Farm and Kitchen ever since. I love to grow our own foods, make
+          healthy delicious meals fand offer fresh, local produce to the Vashon
+          Community
         </p>
       </div>
+      <hr />
     </div>
   );
 }
